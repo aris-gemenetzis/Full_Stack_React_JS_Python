2de9cb8073c2c6c29b123ae4d9ed755fcc79541f
ar3saris <gemenetzisaris@gmail.com>
2025-04-23 01:14:20 +0300

    App.test.js revert

--- front/src/App.test.js
+++ front/src/App.test.js
@@ -1,11 +1,8 @@
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
-
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
-
-test('renders component with enzyme.', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.exists()).toBe(true);
-})
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
